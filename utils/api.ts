fdb3ce620a5bc409f73d8fc48511407800f45f53
Your Name <you@example.com>
2020-12-14 15:38:20 +0800

    20201214-c2-tokenUpdate

--- utils/api.ts
+++ utils/api.ts
@@ -11,7 +11,7 @@ export function initializeAxios(axiosInstance: NuxtAxiosInstance) {
     headers: {
       'Time-Zone': 8,
       'Accept-Language': navigator.language,
-      'Authorization': 'kbmJlOPBjQem/n/5KDhdofR3J0Dz1Txc3ay9uVJfWkHG9vnz7wn4rVLazQsrNqkgbBmPGTgDQ1KlPjs17CWO+3Lwa4YIDaliLU9L7lYGQ9rgE8muMswgPA20KfFij1UNPwYPzRT5UBl3b+7+9yBaMD6ZMilvHTp3KLxD10999FzbBMR9Pr2bpOF71DSZpZmByPHdLkCoehAXpJ1DcOVszFYmev31QlDv+Oidxxw/GqRvfUUuWbuvKP+NmvFQ91MDZbnJxu7CuHnqyYiYfi1XCG+gEzLTqX5fnkbUq4T7ciW92e61NtkSpwcNb7vru4O2oleFVDdHv0cSoxqyexZ6kIdqPSvioc+UImkLkBcI1XEfAvvOe2QB0N2/fbd9hsQvXvdchhoK2julyx2Y9F6xP39Q0I7wiW+PgHGiutbvrSnOfLY8HYC9ofWziOotNmXH'
+      'Authorization': '2+EsjWKj5SD5gtRJjuAOBY3g+TKGnsBGjg347gBV7E65C68jzJ4IqruodBWiQ68S7NAMMRTS0eiq1dpczAmK87M10Jv4uftjFZYi7MOh2i2/MP21C1xksnSAyi5UAQU36r1G77uoAB0jhpWuOL0GcfwLO/Wlwz6SVyW/fZVM7DxBrGH6YEj1lVwFzwcLYSxg36KC1Wz/xkQHepgdYIWKvSSd5pMwXN6i/wcWMMRIT4o/buNHBikuQimvn6WfYQQf/waxm0qJN1AbN6lmwKVgjEMUgLPs+BgXycgbmUEyOlVwB5ajh5UWqI09uA9dqwIzXCDmYLtY+EsgObx43ySEwe9spTzYnoPumIFTOcJ5iNUWQrWqkuceuej+98qvn4PN80bbA3GwyFdfPdXUOORovSu3HUeUMyETlFbPdLaI2P2JxrWvkYwoI73R06W9GWNMAw994nmwUI1B1cz4Ej8vwvQlaw+RerrPIof7w2LwdAKvY87E/hBR9NI/bsG9hp+f5b1cwyihex0xfzk7uhwollcc34lbH89+WlRrPSY2ZcHRACMjBfAsCYxJZjoIoJSvHEqO99sPdIg+B77ChgVXIyHFB/xr5qWyNfOjDHg6b+Z8yLrhUmCAaOVjDsfpuXsNqYoYnz6RX5/yKluMozslEgc6NqJ8CNW9XTddTl5bZWtteKT14NihIVbaool2R8loq8Veaddb0yfjKIZda87J8YdWtZtspqmrNEvuFeWqy5Y='
     },
     baseURL: process.env.API_URL,
     timeout: Number(process.env.TIMEOUT_LIMIT) || 20000
